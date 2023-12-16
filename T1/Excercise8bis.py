d0281288fbab12bd3c6fc87b78d5ee8d1835176b
Pablo Sollo <psolloponce@al.uloyola.es>
2023-12-16 13:39:55 +0100

    a

--- T1/Excercise8bis.py
+++ T1/Excercise8bis.py
@@ -3,15 +3,17 @@
 
 import random
 
+
 def roll_two_dice():
-    #Todo
-    #Todo 
-    return#TodO
+    die1 = random.randint(1, 6)  # Roll the first die
+    die2 = random.randint(1, 6)  # Roll the second die
+    return die1, die2
 
 def main():
- 
     die1, die2 = roll_two_dice()
-    print(f"Sum total: ",die1+die2)
-    
+    print(f"Result of the first die: {die1}")
+    print(f"Result of the second die: {die2}")
+    print(f"Sum total: {die1 + die2}")
+
 if __name__ == "__main__":
     main()
