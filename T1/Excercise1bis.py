03130da9155c48309de28ab18cf4d4c69b92ac98
Pablo Sollo <psolloponce@al.uloyola.es>
2023-12-16 09:25:37 +0100

    exercise

--- T1/Excercise1bis.py
+++ T1/Excercise1bis.py
@@ -1,11 +1,11 @@
 ## Sum of even numbers: Write a program that sums all the even numbers from 1 to 100.
 def calculation():
     # Method 2: Using a while loop
-    result = # TODO: Inizialize variable result
-    number = # TODO:  Inizialize variable result
-    while number <= # TODO: Generate the proper while loop
+    result = 0 #Inizialize variable result
+    number = 2  #Inizialize variable result
+    while number <= 100:     #Generate the proper while loop
         result += number
-        number += # TODO: Update number variable for even.
+        number += 2       # Update number variable for even.
 
     print("Sum of even numbers (Method 2):", result)
 
