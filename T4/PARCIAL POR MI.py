cf5fe81b5440869b139c4e328c636116ac09d102
Pablo Sollo <psolloponce@al.uloyola.es>
2023-12-19 13:31:56 +0100

    parcial por mi

--- T4/PARCIAL POR MI.py
+++ T4/PARCIAL POR MI.py
@@ -1,112 +1,107 @@
 class Publication:
-    def __init__(self, title: str, authors: list[str], year: int, status: str = "available"):
-        self.title = title 
+    def __init__ (self, title:str, authors: list[str], year:int, status:str = "available"):
+        self.title = title
         self.authors = authors
         self.year = year
         self.status = status
-
     def __str__(self):
-        return f"{self.title} - {self.authors} ({self.year})"
-
+        return f" {self.title} - {self.authors} - ({self.year})"
+    
+    
 class Book(Publication):
-    def __init__(self, title: str, authors: list[str], year: int, isbn: str, num_pages: int, status: str = "available"):
-        super().__init__(title, authors, year, status)
-        if len(isbn) == 13 and isbn.isdigit():
+    def __init__ (self, title: str, authors: list[str], year: int, isbn: str, num_pages: int ,status: str = "available"):
+        super().__init__(title,authors,year,status)
+        self.num_pages = num_pages
+        if len(isbn) == 13 and isbn.isdigit:
             self.isbn = isbn
         else:
-            print("ISBN must be a string of length 13 containing only digits")
-        self.num_pages = num_pages
+            print("ISBN must be a string of 13 length.")
+
     
-    def __str__(self):
-        return f"{super().__str__()} "
-
-
-
-
+    
 class Journal(Publication):
-    def __init__ (self, title: str, authors: list[str], year: int, edition_number: int, periodicity: str, status: str = "available"):
+    def __init__(self,title: str,authors: list[str],year: int,edition_number: int,periodicity: str, status: str = "available"):
         super().__init__(title,authors,year,status)
-        self.edition_number= edition_number
-        self.periodicity= periodicity
+        self.edition_number = edition_number
+        self.periodicity = periodicity
+    
+    
 
-   
+
 class User:
-    def __init__(self, name: str, userID: str, max_pubs: int):
+    def __init__(self, name: str, user_id: str, max_pubs: int):
         self.name = name
 
-        if len(userID) == 9 and userID.isdigit():
-            self.userID = userID
+        if len(user_id)==9 and user_id.isdigit():
+            self.user_id = user_id
         else:
-            print("userID must be a string of length 9 containing only digits")
-
+            print("User ID must be a string of length 9.")
+        
         self.pubs = []
         self.max_pubs = max_pubs
 
-    def __str__(self):
-        return f"{self.name} (ID: {self.userID})"
+   
+    
 
     def lend_pub(self, publication):
         if len(self.pubs) < self.max_pubs and publication.status == "available":
             self.pubs.append(publication)
             publication.status = "borrowed"
-            print(f"The Book '{publication.title}' has been lent to {self.name}.")
+            print(f"The Publication '{publication.title}' has been lent to {self.name}.")
         elif len(self.pubs) >= self.max_pubs:
             print(f"{self.name} has reached the maximum limit of borrowed items.")
         else:
-            print(f"The Book '{publication.title}' is already borrowed.")
+            print(f"The Publication '{publication.title}' is already borrowed.")
 
     def return_pub(self, publication):
         if publication in self.pubs and publication.status == "borrowed":
             self.pubs.remove(publication)
             publication.status = "available"
-            print(f"The book '{publication.title}' was returned by {self.name}.")
+            print(f"The Publication '{publication.title}' was returned by {self.name}.")
         else:
             print(f"{self.name} did not borrow '{publication.title}' from this library or it's not currently borrowed.")
 
 
-
-
 class Professor(User):
-    def __init__ (self, name: str, userID: str, department: str, employee_id:str, max_pubs:int = 2):
-        super().__init__(name, userID, max_pubs)
-
+    def __init__(self,name: str, user_id: str, department: str, employee_id: str, max_pubs:int = 2):
+        super().__init__(name,user_id,max_pubs)
         self.department = department
-
+        
         if len(employee_id) == 6:
             self.employee_id = employee_id
         else:
-            print("Employee_id must be a string of length 6.")
+            print("Employee ID must be a string of length 6.")
 
 class Student(User):
-    def __init__(self, name: str, userID: str, grade:str, studentID: str, max_pubs:int = 1):
-        super().__init__(name, userID, max_pubs)
-
+    def __init__(self, name:str, user_id:str, grade:str, student_id:str ,max_pubs:int = 1):
+        super().__init__(name,user_id,max_pubs)
         self.grade = grade
 
-        if len(studentID) == 6:
-            self.studentID= studentID
+        if len(student_id) == 6:
+            self.student_id = student_id
         else:
-            print("StudentID must be a sting of length 6.")
+            print("Employee ID must be a string of length 6.")
+
 
 class Library:
-    def __init__ (self, name):
+    def __init__ (self, name: str):
         self.name = name
         self.catalogue = []
         self.users = []
 
     def show_catalogue(self):
         print(f"Catalogue of the library: {self.name}")
-        print("---------------------------------------")
+        print("--------------------------------------------------------------")
         for pub in self.catalogue:
             print(pub)
-        print("---------------------------------------")
-    
-    def add_publication(self, publication):
+        print("--------------------------------------------------------------")
+
+    def add_publication(self,publication):
         self.catalogue.append(publication)
 
     def register_user(self,user):
         self.users.append(user)
-
+    
     def lend_pub(self,user,publication):
         if user in self.users and publication in self.catalogue:
             user.lend_pub(publication)
@@ -124,6 +119,8 @@ class Library:
             print(f"The user {user.name} is not registered.")
         else:
             print(f"The book {publication.title} is not in the library catalogue.")
+
+
 
 if __name__ == "__main__":
     library = Library("Loyola Andalucía Library")
@@ -145,7 +142,6 @@ if __name__ == "__main__":
     library.add_publication(journal2)
     library.register_user(professor1)
     library.register_user(student1)
-    
     library.show_catalogue()
     library.lend_pub(professor1, book1)
     library.lend_pub(student1, book1) # the book should be borrowed
@@ -157,23 +153,10 @@ if __name__ == "__main__":
     library.lend_pub(student2, journal1) # User not registred
 
 
-
-# publication
     
-# def __str__(self):
-        #return f" {self.title} - {self.authors} - ({self.year})"
-
-#book
     
-    # def __str__(self):
-        #return f" {super().__str__()} ISBN: {self.isbn} Pages: {self.num_pages}"
-    
-#user
-    #    def __str__(self):
-        #return f"{self.name} - (ID: {self.user_id})"
-    
-
     
 
 
-   
+
+    
